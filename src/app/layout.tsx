2f3a639119fd0d8adb0b91868cf88ef0f60f9559
Lucas Souza <108772316+SouzaLucas0@users.noreply.github.com>
2025-07-31 16:36:16 -0400

    :sparkles: feat: Implementado Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import type React from "react"
 import "./globals.css"
+import GoogleAnalytics from "../components/GoogleAnalytics"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body className={inter.className}>
+          <GoogleAnalytics />
           {children}
       </body>
     </html>
